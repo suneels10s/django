28efd098976f5e29b7c39f41db71a8d90ca30c74
Sunil Chakradhar <logodesigny@gmail.com>
2025-08-24 20:37:49 +0545

    Add files via upload

---
+++ crud/crudApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from crudApp.models import *
+
+# Register your models here.
+
+admin.site.register(Employees)
+
+
---
+++ crud/crudApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Employees(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="employees_images/", null=True, blank=True)
+    email = models.EmailField(max_length=100)
+    address = models.TextField()
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.name
